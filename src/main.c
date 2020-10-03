93d40dc8340a0c47f224e658f5f195a979b1cf6a
sergio shimura <shimurasergio@gmail.com>
2020-10-03 18:47:43 -0300

    teste para valor ré de 1000

--- src/main.c
+++ src/main.c
@@ -302,8 +302,8 @@ int main(void) {
         }
 
         if(adc_buffer.l_tx2 < ADC1_ZERO) {
- //         cmd2_ADC = (CLAMP(adc_buffer.l_tx2, ADC1_MIN, ADC1_ZERO) - ADC1_ZERO) / ((ADC1_ZERO - ADC1_MIN) / ADC1_MULT_NEG); // ADC1 - Speed
-		  cmd2_ADC = (CLAMP(adc_buffer.l_tx2, ADC1_MIN, ADC1_ZERO) - ADC1_ZERO) / ((ADC1_MIN - ADC1_ZERO) / ADC1_MULT_NEG); // ADC1 - Speed
+          cmd2_ADC = (CLAMP(adc_buffer.l_tx2, ADC1_MIN, ADC1_ZERO) - ADC1_ZERO) / ((ADC1_ZERO - ADC1_MIN) / ADC1_MULT_NEG); // ADC1 - Speed
+		  cmd2_ADC = -1000; // ADC1 - Speed
         } else {
           cmd2_ADC = (CLAMP(adc_buffer.l_tx2, ADC1_ZERO, ADC1_MAX) - ADC1_ZERO) / ((ADC1_MAX - ADC1_ZERO) / ADC1_MULT_POS); // ADC1 - Speed
         }
